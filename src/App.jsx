e7a5544b5677d408a74ee26caf68295072697f3b
Thomas Abraham <thomas.abraham@fxdigital.uk>
2024-09-18 15:37:41 +0100

    moved the import stmt for GA4 above AppContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import useDarkMode from 'use-dark-mode';
+import ReactGA from 'react-ga4';
 import AppContext from './AppContext';
 import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
 import { lightTheme, darkTheme } from './theme/themes';
-import ReactGA from 'react-ga4';
-
 
 function App() {
   ReactGA.initialize('G-1GRG57RS6J');
